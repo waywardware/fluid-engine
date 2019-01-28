4bea3d6bbff741b254ef9ea907c260d7ab986c29
Angel Bouzo <angelb7574@gmail.com>
2019-01-28 10:59:12 -0800

    reworked exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-export * from "./Core";
-export * from "./Collision";
-export * from "./Input";
-export * from "./Models";
-export * from "./Objects";
-export * from "./Renderer";
+export { ticker$ } from "./Core";
+export {onCollisionEvent$, isColliding,
+    checkForCollision, checkAndFireCollisionEvent, 
+    fireCollisionEvent} from "./Collision";
+export { input$ } from "./Input";
+export { IPoints, ITimeObject, EventType, IEvent,
+            IInitialGameObjectData, IGameObject } from "./Models";
+export { addGameObject, removeGameObject, onAddObjectEvent$, onRemoveObjectEvent$,
+            objects$ } from "./Objects";
+export { DEFAULT_DRAW_SPRITE, initRenderer } from "./Renderer";
